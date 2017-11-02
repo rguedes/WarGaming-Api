52979d243e2942745444a5f4e6a86ba32f72b214
Rolando Guedes <rolando.guedes@3gnt.net>
2017-11-02 11:55:42 +0000

    Add tanks api

---
+++ src/WarGaming/Api/Method/WoT/Encyclopedia/Tanks.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * This file is part of the WarGaming API package
+ *
+ * (c) Vitaliy Zhuk
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ */
+
+namespace WarGaming\Api\Method\WoT\Encyclopedia;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use WarGaming\Api\Annotation\FormData;
+use WarGaming\Api\Method\AbstractMethod;
+use WarGaming\Api\Model\Collection;
+
+/**
+ * Tank info API
+ *
+ * Attention: the processor this this method not return value. All tank info
+ * saved in TankInfo::tanks property!
+ *
+ * @author Vitaliy Zhuk <zhuk2205@gmail.com>
+ */
+class Tanks extends AbstractMethod
+{
+    /**
+     * @var Collection|\WarGaming\Api\Model\WoT\Tank\Tank[]
+     *
+     * @Assert\Type("WarGaming\Api\Model\Collection")
+     * @Assert\Count(
+     *      min = 1,
+     *      max = 100
+     * )
+     * @Assert\All({
+     *      @Assert\Type("WarGaming\Api\Model\WoT\Tank\Tank")
+     * })
+     *
+     * @FormData(name="tank_id", type="list", collectionLoad=true, collectionCacheLoad=true)
+     */
+    public $tanks;
+
+    /**
+     * @var int
+     */
+    public $cacheTtl = 86400; // 1 day
+
+    /**
+     * Construct
+     */
+    public function __construct()
+    {
+        $this->tanks = new Collection();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isCacheAvailable()
+    {
+        return true;
+    }
+}
---
+++ src/WarGaming/Api/Method/WoT/Encyclopedia/TanksProcessor.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the WarGaming API package
+ *
+ * (c) Vitaliy Zhuk
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ */
+
+namespace WarGaming\Api\Method\WoT\Encyclopedia;
+
+use Guzzle\Http\Message\Response as GuzzleResponse;
+use WarGaming\Api\Method\AbstractProcessor;
+use WarGaming\Api\Method\MethodInterface;
+
+/**
+ * Tank info API processor
+ *
+ * @author Vitaliy Zhuk <zhuk2205@gmail.com>
+ */
+class TanksProcessor extends AbstractProcessor
+{
+    /**
+     * Get API Uri
+     *
+     * @return string
+     */
+    protected function getApiUri()
+    {
+        return 'wot/encyclopedia/tankinfo';
+        https://api.worldoftanks.ru/wot/encyclopedia/tanks/?application_id=cba9bfbfc93ed03d2ee25efe54a6aec6
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function parseResponse(array $data, array $fullData, GuzzleResponse $response, MethodInterface $method)
+    {
+        /** @var TankInfo $method */
+        foreach ($method->tanks as $index => $tank) {
+            if (!empty($data[$tank->id])) {
+                $tank->setFullDataFromArray($data[$tank->id]);
+            } else {
+                unset ($method->tanks[$index]);
+            }
+        }
+    }
+}
